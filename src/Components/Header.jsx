053c1e3f676d7765bf62380691026b61166d979d
oranjevo <iandrew.jj@gmail.com>
2021-10-15 14:14:06 +0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        className="logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png"
+        alt="logo"
+      />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,19 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div alt="headimg" className="content-header-img" />
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New Post</div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+      Main content
+    </div>
+  )
+}
+
+export default Profile
